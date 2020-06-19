11bbeeaccf03c6ab493a331f981d2d8893426f24
bearhsiang <b05902066@ntu.edu.tw>
2020-06-19 11:55:29 +0800

    add acrithmetic

---
+++ arithmetic/arith.py
@@ -0,0 +1,10 @@
+a = 10
+b = 20
+
+print(a, '+', b, '=', a+b)
+print(a, '-', b, '=', a-b)
+print(a, '*', b, '=', a*b)
+print(a, '/', b, '=', a/b)
+print(a, '%', b, '=', a%b)
+print(a, '**', b, '=', a**b)
+print(a, '//', b, '=', a//b)
