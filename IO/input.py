889cc87f188af34e4d78bdd506f6dcc8956c1a3e
bearhsiang <b05902066@ntu.edu.tw>
2020-06-19 12:48:02 +0800

    add code

---
+++ IO/input.py
@@ -0,0 +1,6 @@
+num = input("please input a number: ")
+print(type(num), num)
+print(type(int(num)), 'int('+str(num)+')')
+
+multiple_num = input("please input multiple numbers: ")
+print(type(multiple_num), multiple_num)
--- variables-and-IO/input.py
+++
@@ -1,3 +0,0 @@
-num = input("please input a number: ")
-print(type(num), num)
-print(type(int(num)), 'int('+str(num)+')')
