244e1d2068255e40ab4b467510f9a2cc3fbd67ba
sean <b05902066@ntu.edu.tw>
2020-07-01 10:39:37 +0800

    add code

--- weekly_progress/w1.py
+++ weekly_progress/w1.py
@@ -1,12 +1,16 @@
-ans = 666
+start = 0
+end = 100
+ans = int(input('please input the ans ('+str(start)+'~'+str(end)+'): '))
 count = 0
 while True:
 	print('round', count)
-	guess = int(input("guess a number: "))
+	guess = int(input('guess a number ('+str(start)+'~'+str(end)+'): '))
 	if guess > ans:
 		print('lower')
+		end = guess
 	elif guess < ans:
 		print('higher')
+		start = guess
 	else:
 		print('hit')
 		break
