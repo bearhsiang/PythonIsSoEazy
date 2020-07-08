ae2556f664c6ae9e2cd1a0d258b6352f23626bb3
sean <b05902066@ntu.edu.tw>
2020-07-08 10:47:39 +0800

    add hw

---
+++ hw/jul_7.py
@@ -0,0 +1,38 @@
+while True:
+    print('')
+    print('\
+  ____                     _   _                 \n\
+ / ___|_   _  ___  ___ ___| \ | |_   _ _ __ ___  \n\
+| |  _| | | |/ _ \/ __/ __|  \| | | | | \'_ ` _ \ \n\
+| |_| | |_| |  __/\__ \__ \ |\  | |_| | | | | | | \n\
+ \____|\__,_|\___||___/___/_| \_|\__,_|_| |_| |_| \n\
+    ')
+    print('1. Start game')
+    print('2. quiz')
+
+    op = input('>>> ')
+    if op == '1':
+        start = 0
+        end = 100
+        ans = int(input('please input the ans ('+str(start)+'~'+str(end)+'): '))
+        count = 0
+        while True:
+            print('round', count)
+            guess = int(input('guess a number ('+str(start)+'~'+str(end)+'): '))
+            if guess > ans:
+                print('lower')
+                end = guess
+            elif guess < ans:
+                print('higher')
+                start = guess
+            else:
+                print('hit')
+                break
+            count += 1
+            print()
+
+        print('yes, my ans is', ans)
+        print('you use', count, 'round')
+
+    if op == '2':
+        break
