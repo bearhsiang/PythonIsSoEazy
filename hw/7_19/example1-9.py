89d62a737834662a003b1b54229949a39c70bb7a
sean <b05902066@ntu.edu.tw>
2020-07-26 01:37:55 +0800

    add code

---
+++ hw/7_19/example1-9.py
@@ -0,0 +1,42 @@
+# 1
+colors = ['yello', 'red', 'orange']
+
+# 2
+fruits = ['apple', 'orange', 'banana', 'lemon', 'tomato']
+print(fruits[1])
+
+# 3
+animals = ['bird', 'bear', 'dog', 'cat', 'rabbit', 'cow', 'horse']
+print(len(animals))
+animals.append('elephant')
+
+# 4
+numbers = [5, 1, 2, 7, 4]
+numbers.sort()
+
+# 5
+constellations = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagirrarius', 'capricorn', 'aquarius', 'pisces']
+constellations.sort()
+constellations.reverse()
+
+# 6
+friends = ['eugene', 'veronica', 'sheena']
+for name in friends:
+    print(name)
+
+# 7
+color1 = ['red', 'blue', 'green']
+color2 = ['orange', 'purple', 'white']
+color_all = color1 + color2
+
+# 8
+numbers = [4, 3, 2, 5, 1, 5]
+Num = numbers[1:5]
+
+# 9
+animals = ['dog', 'cat', 'cow', 'horse', 'frog']
+if 'monkey' in animals:
+    print('I have monkey.')
+else:
+    print('I don\'t have monkey.')
+
